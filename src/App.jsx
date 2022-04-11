f3211ee0d8d218a4c7947a042aee688f1c56ec2b
apuopx <apuopx@gmail.com>
2022-04-11 18:39:34 +0300

    пробую всяке

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,103 @@
 import './App.css';
 
 function App() {
-  const questions= [
+  const questions = [
     {
-        id:'1',   
-        question:'Виды инструктажей, проводимые для рабочих профессий:',
-        true:'1',
-        answers:[
-            'По программе первичного инструктажа для работников рабочих профессий',
-            'По программе вводного инструктажа',
-            'Программа не требуется'
-        ]
+      id: '1',
+      question: 'Виды инструктажей, проводимые для рабочих профессий:',
+      true: '1',
+      answers: {
+        1: 'По программе первичного инструктажа для работников рабочих профессий',
+        2: 'По программе вводного инструктажа',
+        3: 'Программа не требуется',
+      },
     },
-    {
-        id:'2',
-        question:'question2',
-        true:'1',
-        answers:[
-            '4',
-            '5',
-            '6'
-        ]
-    },
-    {
-        id:'3',
-        question:'question3',
-        true:'1',
-        answers:[
-            '7',
-            '8',
-            '9'
-        ]
-    },
-    {
-        id:'4',
-        question:'question4',
-        true:'1',
-        answers:[
-            '10',
-            '11',
-            '12'
-        ]
-    },
-    {
-        id:'5',
-        question:'question5',
-        true:'1',
-        answers:[
-            '13',
-            '14',
-            '15'
-        ]
-    },
-    {
-        id:'6',
-        question:'question6',
-        true:'1',
-        answers:[
-            '16',
-            '17',
-            '18'
-        ]
-    },
-    ]
-    
-    let rand = Math.floor(Math.random() * questions.length);
-    let questionRand = questions[rand].question
-    let id = questions[rand].id
-    let answers = questions[rand].answers
-    let answerRand= answers.sort(()=>Math.random()-0.5)
-    let count = 1
+    // {
+    //     id:'2',
+    //     question:'question2',
+    //     true:'1',
+    //     answers:[
+    //         '4',
+    //         '5',
+    //         '6'
+    //     ]
+    // },
+    // {
+    //     id:'3',
+    //     question:'question3',
+    //     true:'1',
+    //     answers:[
+    //         '7',
+    //         '8',
+    //         '9'
+    //     ]
+    // },
+    // {
+    //     id:'4',
+    //     question:'question4',
+    //     true:'1',
+    //     answers:[
+    //         '10',
+    //         '11',
+    //         '12'
+    //     ]
+    // },
+    // {
+    //     id:'5',
+    //     question:'question5',
+    //     true:'1',
+    //     answers:[
+    //         '13',
+    //         '14',
+    //         '15'
+    //     ]
+    // },
+    // {
+    //     id:'6',
+    //     question:'question6',
+    //     true:'1',
+    //     answers:[
+    //         '16',
+    //         '17',
+    //         '18'
+    //     ]
+    // },
+  ];
 
+  let rand = Math.floor(Math.random() * questions.length);
+  let questionRand = questions[rand].question;
+  let id = questions[rand].id;
+  let answers = Object.values(questions[rand].answers);
+//   let answersKye = Object.keys(questions[rand].answers);
+  let answerRand = answers.sort(() => Math.random() - 0.5);
+  let count = 1;
 
-let question = ()=>{
-        return <div>{id}) Вопрос: {questionRand}</div>
-      }
-let answerShow = answerRand.map((item)=>{
-    return <div><span>{count++}) </span><label ><input type="radio" name="answer" id='{count++}' /><span>{item}</span></label></div>
-})
- 
+  let question = () => {
+    return (
+      <div>
+        {id}) Вопрос: {questionRand}
+      </div>
+    );
+  };
+  let answerShow = answerRand.map((item) => {
+    return (
+      <div>
+        <span>{count++}) </span>
+        <label>
+          <input type='radio' name='answer' id='{count++}' />
+          <span>{item}</span>
+        </label>
+      </div>
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        
+    <div className='App'>
+      <header className='App-header'>
         <div>
-        {question(questionRand)}
-        {answerShow}
+          {question(questionRand)}
+          {answerShow}
         </div>
-
-
       </header>
     </div>
   );
