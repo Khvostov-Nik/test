d714d4b2a6a32e491ccb2f27a6756fc1bf224a0a
apuopx <apuopx@gmail.com>
2022-04-16 16:15:17 +0300

    решил почти все проблемы что были раньше

--- src/App.jsx
+++ src/App.jsx
@@ -1,104 +1,113 @@
-import "./App.css";
+import './App.css';
 
 function App() {
   const questions = [
     {
-      id: "1",
-      question: "Виды инструктажей, проводимые для рабочих профессий:",
-      true: "1",
+      id: '1',
+      question: 'Виды инструктажей, проводимые для рабочих профессий:',
+      true: '1',
       answers: {
-        a: "По программе первичного инструктажа для работников рабочих профессий",
-        b: "По программе вводного инструктажа",
-        c: "Программа не требуется",
+        a: 'По программе первичного инструктажа для работников рабочих профессий',
+        b: 'По программе вводного инструктажа',
+        c: 'Программа не требуется',
       },
     },
     {
-      id: "2",
-      question: "question2",
-      true: "2",
+      id: '2',
+      question: 'question2',
+      true: '2',
       answers: {
-        a: "4",
-        b: "5",
-        c: "6",
+        a: 'Lorem ipsum dolor sit amet.',
+        b: 'em ipsum dolor sit amet.',
+        c: '6Lorem ipsum nnndolor sit amet.',
       },
     },
     {
-      id: "3",
-      question: "question3",
-      true: "1",
+      id: '3',
+      question: 'question3',
+      true: '1',
       answers: {
-        a: "7",
-        b: "8",
-        c: "9",
+        a: '7',
+        b: '8',
+        c: '9',
       },
     },
     {
-      id: "4",
-      question: "question4",
-      true: "2",
+      id: '4',
+      question: 'question4',
+      true: '2',
       answers: {
-        a: "10",
-        b: "11",
-        c: "12",
+        a: '10',
+        b: '11',
+        c: '12',
       },
     },
     {
-      id: "5",
-      question: "question5",
-      true: "3",
+      id: '5',
+      question: 'question5',
+      true: '3',
       answers: {
-        a: "13",
-        b: "14",
-        c: "15",
+        a: '13',
+        b: '14',
+        c: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores temporibus vero rerum. Qui voluptate beatae nobis pariatur sunt esse magnam in commodi explicabo expedita enim unde optio modi perferendis amet, autem soluta animi eligendi veritatis facilis! Ex dignissimos velit autem nobis earum ducimus, eos beatae, accusantium, sunt qui explicabo corporis!',
       },
     },
     {
-      id: "6",
-      question: "question6",
-      true: "2",
+      id: '6',
+      question: 'question6',
+      true: '2',
       answers: {
-        a: "16",
-        b: "17",
-        c: "18",
+        a: '16',
+        b: '17',
+        c: '18',
       },
     },
   ];
-
-  let rand = Math.floor(Math.random() * questions.length);
-  let questionRand = questions[rand].question;
-  let id = questions[rand].id;
-  // let trueAnswer = questions[rand].true;
-  let answers = Object.entries(questions[rand].answers);
-  let answerRand = answers.sort(() => Math.random() - 0.5);
+  let count = 1;
+  const rand = Math.floor(Math.random() * questions.length);
+  const questionRand = questions[rand].question;
+  const id = questions[rand].id;
+  const answers = Object.entries(questions[rand].answers);
+  const answerRand = answers.sort(() => Math.random() - 0.5);
   
-  let question = () => {
+  const checkAnswer = () => {
+    const checkRadio =document.querySelector(
+      'input[name="answer"]:checked'
+      );
+    // const trueAnswer = questions[rand].true;
+      if (!checkRadio) {
+        
+        return
+    }
+  };
+
+  const question = () => {
     return (
-      <div key={id}>
+      <div className='question' key={id}>
         {id}) Вопрос: {questionRand}
       </div>
     );
   };
 
-//  const getValue =()=> document.querySelector('input[name="answer"]:checked').value;
-
-  let answerShow = answerRand.map((item) => {
+  const answerShow = answerRand.map((item) => {
     return (
-      <div key={item[0]}>
-        <label>
-          <input value={item[0]} type="radio" name="answer" id="{count++}" />
-          <span > {item[1]} </span>
+      <div className='answer'>
+        <label className='answer-inner'>
+          <input className='input-radio' value={item[0]} type='radio' name='answer' />
+          <div>{count++})</div> 
+          <div className='answer-text'>{item[1]} </div>
         </label>
       </div>
     );
   });
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <div>
+    <div className='App'>
+      <header className='App-header'>
+        <div class='container'>
           {question(questionRand)}
-          {answerShow }
-          <button>Проверить ответ</button>
+          {answerShow}
+          <button onClick={checkAnswer} className='btn'>Проверить ответ</button>
         </div>
       </header>
     </div>
